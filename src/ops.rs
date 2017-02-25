0fd676e84c931bee27e44ad14c71b6bbfe7f2df7
Willie Zutz <boarder2@bit-shift.com>
2017-02-25 00:10:13 -0700

    jt, jf instructions

--- src/ops.rs
+++ src/ops.rs
@@ -11,18 +11,22 @@ pub fn run_op(current_instruction: u16,
 	match memory[current_instruction as usize] {
 		0 => return None,
 		1 => {
-			set(registers,
-			    memory[current_instruction as usize + 1],
-			    memory[current_instruction as usize + 2]);
+			set(current_instruction, memory, registers);
 			new_instruction_index += 2;
 		}
 		6 => return Some(jmp(current_instruction, memory, registers)),
+		7 => return Some(jt(current_instruction, memory, registers)),
+		8 => return Some(jf(current_instruction, memory, registers)),
 		19 => {
 			out(memory[current_instruction as usize + 1]);
 			new_instruction_index += 1;
 		}
 		21 => {} // 21 is NOOP, so just move to the next instruction.
-		i => println!("Instruction not implemented {:?} at offset {:?}", i, current_instruction),
+		i => {
+			println!("Instruction not implemented {:?} at offset {:?}",
+			         i,
+			         current_instruction)
+		}
 		//_ => {}
 	}
 
@@ -34,18 +38,37 @@ fn out(code: u16) {
 	print!("{}", code as u8 as char);
 }
 
-fn set(registers: &mut Vec<u16>, register: u16, value: u16) {
-	println!("Register set {:?} to {:?} - Registers {:?}",
-	         register,
-	         value,
-	         registers);
+fn set(current_instruction: u16, memory: &Vec<u16>, registers: &mut Vec<u16>) {
+	let register = memory[current_instruction as usize + 1] - 32768;
+	let value = get_mem_or_register_value(memory[current_instruction as usize + 2], registers);
 	if let Some(r) = registers.get_mut(register as usize) {
+		println!("Setting register {:?} to {} previous value {}",
+		         register,
+		         value,
+		         r);
 		*r = value;
 	}
+	println!("New registers {:?}", registers);
 }
 
 fn jmp(current_instruction: u16, memory: &Vec<u16>, registers: &Vec<u16>) -> u16 {
 	get_mem_or_register_value(memory[current_instruction as usize + 1], registers)
+}
+
+fn jt(current_instruction: u16, memory: &Vec<u16>, registers: &Vec<u16>) -> u16 {
+	let val = get_mem_or_register_value(memory[current_instruction as usize + 1], registers);
+	if val != 0 {
+		return get_mem_or_register_value(memory[current_instruction as usize + 2], registers);
+	}
+	current_instruction + 3
+}
+
+fn jf(current_instruction: u16, memory: &Vec<u16>, registers: &Vec<u16>) -> u16 {
+	let val = get_mem_or_register_value(memory[current_instruction as usize + 1], registers);
+	if val == 0 {
+		return get_mem_or_register_value(memory[current_instruction as usize + 2], registers);
+	}
+	current_instruction + 3
 }
 
 fn get_mem_or_register_value(memory_value: u16, registers: &Vec<u16>) -> u16 {
--- src/vm.rs
+++ src/vm.rs
@@ -3,6 +3,11 @@ use ops;
 pub fn run(memory: Vec<u16>) {
 	let mut current_op = 0;
 	let mut registers = Vec::with_capacity(8);
+	for _ in 0..8 {
+		 registers.push(0);
+	}
+	let foo = memory.clone();
+	println!("{:?}", foo);
 	while let Some(new_op) = ops::run_op(current_op, &memory, &mut registers) {
 		current_op = new_op;
 	}

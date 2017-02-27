72c6036391a5c9f34942f4794e287bc67ad2ba95
Willie Zutz <boarder2@bit-shift.com>
2017-02-26 23:37:25 -0700

    Implemented call.

--- src/ops/call.rs
+++ src/ops/call.rs
@@ -1,3 +1,4 @@
+use ops;
 use ops::operation::Operation;
 pub struct Call;
 
@@ -6,9 +7,38 @@ impl Operation for Call {
 		2
 	}
 	fn is_jump(&self) -> bool {
-		false
+		true
 	}
-	fn run(&self, _: u16, _: &Vec<u16>, _: &mut Vec<u16>, _: &mut Vec<u16>) -> usize {
-		0
+	fn run(&self, ci: u16, mem: &Vec<u16>, reg: &mut Vec<u16>, stack: &mut Vec<u16>) -> usize {
+		stack.push(ci + 2);
+		ops::get_mem_or_register_value(mem[ci as usize + 1], reg) as usize
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn call_mem() {
+		let op = Call;
+		let expected = 1234;
+		let mem = vec![0, expected];
+		let mut stack = vec![];
+		let result = op.run(0, &mem, &mut Vec::new(), &mut stack);
+		assert_eq!(expected as usize, result);
+		assert_eq!(stack[0], 2);
+	}
+
+	#[test]
+	fn call_reg() {
+		let op = Call;
+		let expected = 1234;
+		let mem = vec![0, 32768];
+		let mut stack = vec![];
+		let mut reg = vec![expected, 0, 0, 0, 0, 0, 0, 0];
+		let result = op.run(0, &mem, &mut reg, &mut stack);
+		assert_eq!(expected as usize, result);
+		assert_eq!(stack[0], 2);
 	}
 }
--- src/ops/ret.rs
+++ src/ops/ret.rs
@@ -3,12 +3,27 @@ pub struct Ret;
 
 impl Operation for Ret {
 	fn len(&self) -> usize {
-		1
+		2
 	}
 	fn is_jump(&self) -> bool {
-		false
+		false//true
 	}
 	fn run(&self, _: u16, _: &Vec<u16>, _: &mut Vec<u16>, _: &mut Vec<u16>) -> usize {
-		0
+		0//stack.pop().unwrap() as usize
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[ignore]
+	#[test]
+	fn ret() {
+		let op = Ret;
+		let expected = 1234;
+		let mut stack = vec![3333,expected];
+		let result = op.run(0, &Vec::new(), &mut Vec::new(), &mut stack);
+		assert_eq!(expected as usize, result);
 	}
 }

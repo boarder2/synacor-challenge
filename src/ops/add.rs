8590271c14fb83822830b518d5642813a79a88b9
Willie Zutz <boarder2@bit-shift.com>
2017-02-26 19:31:06 -0700

    Add test for rollover.

--- src/ops/add.rs
+++ src/ops/add.rs
@@ -12,7 +12,7 @@ impl Operation for Add {
 	fn run(&self, ci: u16, mem: &Vec<u16>, reg: &mut Vec<u16>, _: &mut Vec<u16>) -> usize {
 		let val1 = ops::get_mem_or_register_value(mem[ci as usize + 2], reg);
 		let val2 = ops::get_mem_or_register_value(mem[ci as usize + 3], reg);
-		let new_val = ((val1 as u32 + val2 as u32) % 32768) as u16;
+		let new_val = ((val1 + val2 ) % 32768) as u16;
 		ops::set_register(mem[ci as usize + 1], reg, new_val);
 		0
 	}
@@ -27,6 +27,16 @@ mod tests {
 		let a = Add;
 		let mem = vec![9, 32768, 1111, 1111];
 		let expected = vec![2222, 0, 0, 0, 0, 0, 0, 0];
+		let mut registers = vec![0, 0, 0, 0, 0, 0, 0, 0];
+		a.run(0, &mem, &mut registers, &mut Vec::new());
+		assert_eq!(expected, registers);
+	}
+
+	#[test]
+	fn add_mem_rollover() {
+		let a = Add;
+		let mem = vec![9, 32768, 32767, 32767];
+		let expected = vec![32766, 0, 0, 0, 0, 0, 0, 0];
 		let mut registers = vec![0, 0, 0, 0, 0, 0, 0, 0];
 		a.run(0, &mem, &mut registers, &mut Vec::new());
 		assert_eq!(expected, registers);

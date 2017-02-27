6dcc1bcfca39db9067b47f82902bfcd131b6b872
Willie Zutz <boarder2@bit-shift.com>
2017-02-26 20:35:50 -0700

    Implement push and pop.

--- src/ops/pop.rs
+++ src/ops/pop.rs
@@ -1,3 +1,4 @@
+use ops;
 use ops::operation::Operation;
 pub struct Pop;
 
@@ -8,7 +9,25 @@ impl Operation for Pop {
 	fn is_jump(&self) -> bool {
 		false
 	}
-	fn run(&self, _: u16, _: &Vec<u16>, _: &mut Vec<u16>, _: &mut Vec<u16>) -> usize {
+	fn run(&self, ci: u16, mem: &Vec<u16>, reg: &mut Vec<u16>, stack: &mut Vec<u16>) -> usize {
+		ops::set_register(mem[ci as usize + 1], reg, stack.pop().unwrap());
 		0
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn pop() {
+		let op = Pop;
+		let mem = vec![2, 32768];
+		let expected = vec![1234, 0, 0, 0, 0, 0, 0, 0];
+		let mut registers = vec![1, 0, 0, 0, 0, 0, 0, 0];
+		let mut stack = vec![1234];
+		op.run(0, &mem, &mut registers, &mut stack);
+		assert_eq!(expected, registers);
+		assert_eq!(stack, vec![]);
 	}
 }
--- src/ops/push.rs
+++ src/ops/push.rs
@@ -1,3 +1,4 @@
+use ops;
 use ops::operation::Operation;
 pub struct Push;
 
@@ -8,7 +9,36 @@ impl Operation for Push {
 	fn is_jump(&self) -> bool {
 		false
 	}
-	fn run(&self, _: u16, _: &Vec<u16>, _: &mut Vec<u16>, _: &mut Vec<u16>) -> usize {
+	fn run(&self, ci: u16, mem: &Vec<u16>, reg: &mut Vec<u16>, stack: &mut Vec<u16>) -> usize {
+		let val = ops::get_mem_or_register_value(mem[ci as usize + 1], reg);
+		stack.push(val);
 		0
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn push_mem() {
+		let op = Push;
+		let mem = vec![2, 1234];
+		let expected = vec![1234];
+		let mut registers = vec![1, 0, 0, 0, 0, 0, 0, 0];
+		let mut stack = Vec::new();
+		op.run(0, &mem, &mut registers, &mut stack);
+		assert_eq!(expected, stack);
+	}
+
+	#[test]
+	fn push_reg() {
+		let op = Push;
+		let mem = vec![2, 32768];
+		let expected = vec![1234];
+		let mut registers = vec![1234, 0, 0, 0, 0, 0, 0, 0];
+		let mut stack = Vec::new();
+		op.run(0, &mem, &mut registers, &mut stack);
+		assert_eq!(expected, stack);
 	}
 }

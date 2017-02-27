166f0a310f81efd59e2295abf722f7386f8a878d
Willie Zutz <boarder2@bit-shift.com>
2017-02-26 22:39:16 -0700

    Implement bitwise or.

--- src/ops/or.rs
+++ src/ops/or.rs
@@ -1,3 +1,4 @@
+use ops;
 use ops::operation::Operation;
 pub struct Or;
 
@@ -8,7 +9,56 @@ impl Operation for Or {
 	fn is_jump(&self) -> bool {
 		false
 	}
-	fn run(&self, _: u16, _: &Vec<u16>, _: &mut Vec<u16>, _: &mut Vec<u16>) -> usize {
+	fn run(&self, ci: u16, mem: &Vec<u16>, reg: &mut Vec<u16>, _: &mut Vec<u16>) -> usize {
+		let val1 = ops::get_mem_or_register_value(mem[ci as usize + 2], reg);
+		let val2 = ops::get_mem_or_register_value(mem[ci as usize + 3], reg);
+		let new_val = val1 | val2 as u16;
+		ops::set_register(mem[ci as usize + 1], reg, new_val);
 		0
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn or_mem_on() {
+		let op = Or;
+		let mem = vec![9, 32768, 2, 6];
+		let expected = vec![6, 0, 0, 0, 0, 0, 0, 0];
+		let mut registers = vec![0, 0, 0, 0, 0, 0, 0, 0];
+		op.run(0, &mem, &mut registers, &mut Vec::new());
+		assert_eq!(expected, registers);
+	}
+
+	#[test]
+	fn and_mem_off() {
+		let op = Or;
+		let mem = vec![9, 32768, 2, 4];
+		let expected = vec![6, 0, 0, 0, 0, 0, 0, 0];
+		let mut registers = vec![0, 0, 0, 0, 0, 0, 0, 0];
+		op.run(0, &mem, &mut registers, &mut Vec::new());
+		assert_eq!(expected, registers);
+	}
+
+	#[test]
+	fn and_reg_on() {
+		let op = Or;
+		let mem = vec![9, 32768, 32769, 32770];
+		let expected = vec![6, 2, 6, 0, 0, 0, 0, 0];
+		let mut registers = vec![0, 2, 6, 0, 0, 0, 0, 0];
+		op.run(0, &mem, &mut registers, &mut Vec::new());
+		assert_eq!(expected, registers);
+	}
+
+	#[test]
+	fn and_reg_off() {
+		let op = Or;
+		let mem = vec![9, 32768, 32769, 32770];
+		let expected = vec![6, 2, 4, 0, 0, 0, 0, 0];
+		let mut registers = vec![0, 2, 4, 0, 0, 0, 0, 0];
+		op.run(0, &mem, &mut registers, &mut Vec::new());
+		assert_eq!(expected, registers);
 	}
 }

d92ea4bc9d159a3ed0cee20e2249ff868576511f
Willie Zutz <boarder2@bit-shift.com>
2017-03-01 00:12:57 -0700

    Refactor debugging.

--- src/debugger.rs
+++ src/debugger.rs
@@ -10,12 +10,9 @@ pub fn step(debug_state: &mut debug_state::DebugState,
 	if !debug_state.is_stepping() && !debug_state.is_instruction_break(ci) {
 		return;
 	}
-	print!("{}[2J", 27 as char);
+	clear_term();
 	println!("Output:\n{}", output);
-	println!("NextMem {:?}",
-	         mem.into_iter().skip(ci as usize).take(4).collect::<Vec<&mut u16>>());
-	println!("Registers {:?}", reg);
-	println!("Stack {:?}", stack);
+	print_summary(ci, mem, reg, stack);
 	loop {
 		println!("debug ci:{} - stepping:{}>", ci, debug_state.is_stepping());
 		let mut buf = String::new();
@@ -33,6 +30,9 @@ pub fn step(debug_state: &mut debug_state::DebugState,
 						}
 					}
 				}
+				if args[0] == "summary" {
+					print_summary(ci, mem, reg, stack);
+				}
 				if args[0] == "step" {
 					debug_state.set_stepping(true);
 					break;
@@ -45,29 +45,50 @@ pub fn step(debug_state: &mut debug_state::DebugState,
 					println!("Memory {:?}", mem);
 				}
 				if args[0] == "mem" {
-					if args.len() == 3 {
-						let start = args[1].parse::<i32>().unwrap_or(0) + ci as i32;
-						let len = (ci as i32 + args[2].parse().unwrap_or(0)) - ci as i32;
-						println!("Memory start {:?} end {:?}", start, len + start);
-						println!("{:?}",
-						         mem.into_iter()
-							         .skip(start as usize)
-							         .take(len as usize)
-							         .collect::<Vec<&mut u16>>());
-					} else {
-						println!("Invalid instruction");
-					}
+					set_mem(ci, mem, &args);
 				}
 				if args[0] == "help" {
-					println!("Available commands:");
-					println!("aib");
-					println!("step");
-					println!("cont");
-					println!("dumpmem");
-					println!("mem <begin> <end> (relative to current instruction position)");
+					print_help();
 				}
 			}
 			Err(_) => break,
 		}
 	}
+}
+
+fn clear_term() {
+	print!("{}[2J", 27 as char);
+}
+
+fn print_summary(ci: u16, mem: &mut Vec<u16>, reg: &Vec<u16>, stack: &Vec<u16>) {
+	println!("NextMem {:?}",
+	         mem.into_iter().skip(ci as usize).take(4).collect::<Vec<&mut u16>>());
+	println!("Registers {:?}", reg);
+	println!("Stack {:?}", stack);
+}
+
+fn set_mem(ci: u16, mem: &mut Vec<u16>, args: &Vec<&str>) {
+	if args.len() == 3 {
+		let start = args[1].parse::<i32>().unwrap_or(0) + ci as i32;
+		let len = (ci as i32 + args[2].parse().unwrap_or(0)) - ci as i32;
+		println!("Memory start {:?} end {:?}", start, len + start);
+		println!("{:?}",
+		         mem.into_iter()
+			         .skip(start as usize)
+			         .take(len as usize)
+			         .collect::<Vec<&mut u16>>());
+	} else {
+		println!("Invalid instruction");
+	}
+}
+
+fn print_help() {
+	// clear_term();
+	println!("Available commands:");
+	println!("\tsummary");
+	println!("\taib");
+	println!("\tstep");
+	println!("\tcont");
+	println!("\tdumpmem");
+	println!("\tmem <begin> <end> (relative to current instruction position)");
 }
